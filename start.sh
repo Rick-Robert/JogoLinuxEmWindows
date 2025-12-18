577afa2b6fbd3a7cebbcab04f0c34bb16a5f99c8
Rick-Robert <rickrrtr@gmail.com>
2025-12-18 20:17:03 -0300

    start added

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+# GOG.com (www.gog.com)
+# Game
+
+# Initialization 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+cd "${CURRENT_DIR}" 
+source support/gog_com.shlib 
+
+# Game info 
+GAME_NAME="$(get_gameinfo 1)" 
+VERSION="$(get_gameinfo 2)" 
+VERSION_DEV="$(get_gameinfo 3)" 
+
+# Actions
+run_game() { 
+echo "Running ${GAME_NAME}" 
+cd "${CURRENT_DIR}/game" 
+chmod +x * 
+ ./"Hollow Knight" -force-opengl 
+
+}
+default() { 
+run_game
+} 
+
+# Options
+define_option "-s" "--start" "start ${GAME_NAME}" "run_game" "$@" 
+
+# Defaults
+standard_options "$@" 
